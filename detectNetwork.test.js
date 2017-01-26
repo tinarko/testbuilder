19ea870124dfd3c1711a06a82c9ae880d93c5c40
Tina Ko <tinarchan@gmail.com>
2017-01-26 14:18:25 -0800

    Fix all bugs with Discover and Maestro tests

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -106,18 +106,21 @@ describe('MasterCard', function() {
   // Expect syntax is one way to do this, but there are others. 
   // If you want to know more, check out the documentation. 
   //   http://chaijs.com/api/bdd/
-  var expect = chai.expect;
+
+  //var expect = chai.expect;
  
+  var should = chai.should();
+
   it('has a prefix of 51 and a length of 16', function() {
-    expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
+    detectNetwork('5112345678901234').should.equal('MasterCard');
   });
  
   it('has a prefix of 52 and a length of 16', function() {
-    expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
+    detectNetwork('5212345678901234').should.equal('MasterCard');
   });
  
   it('has a prefix of 53 and has a length of 16', function() {
-    expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
+    detectNetwork('5312345678901234').should.equal('MasterCard');
   });
  
 
@@ -128,7 +131,6 @@ describe('MasterCard', function() {
   // and should, but that's just for learning), so once you've gotten 
   // these tests to pass using should syntax, refactor your tests to 
   // use either expect or should, but not both. 
-  var should = chai.should();
   
   it('has a prefix of 54 and a length of 16', function() {
     detectNetwork('5412345678901234').should.equal('MasterCard');
@@ -140,69 +142,24 @@ describe('MasterCard', function() {
 });
 
 describe('Discover', function() {
-  /*
   // Tests without a function will be marked as "pending" and not run
   // Implement these tests (and others) and make them pass!
+
+
+
   for (var prefix = 644; prefix <= 649; prefix++) {
     (function(prefix) {
 
       it('has a prefix of ' + prefix + ' and a length of 16', function(){
-        console.log(prefix);
         detectNetwork(prefix.toString().concat('1234567890123')).should.equal('Discover');
-        console.log(prefix.toString().concat('1234567890123'));
       });
 
       it('has a prefix of ' + prefix + ' and a length of 19', function(){
-        console.log(prefix);
         detectNetwork(prefix.toString().concat('1234567890123456')).should.equal('Discover');
-        console.log(prefix.toString().concat('1234567890123456'));
       });
 
     })(prefix)
   };
-  */
-  // test to see if these work
-
-  it('has a prefix of 644 and a length of 16', function(){
-      detectNetwork('6441234567890123').should.equal('Discover');
-  });
-  it('has a prefix of 644 and a length of 19', function(){
-      detectNetwork('6441234567890123456').should.equal('Discover');
-  });
-  it('has a prefix of 645 and a length of 16', function(){
-      detectNetwork('6451234567890123').should.equal('Discover');
-  });
-  it('has a prefix of 645 and a length of 19', function(){
-      detectNetwork('6451234567890123456').should.equal('Discover');
-  });
-  it('has a prefix of 646 and a length of 16', function(){
-      detectNetwork('6461234567890123').should.equal('Discover');
-  });
-  it('has a prefix of 646 and a length of 19', function(){
-      detectNetwork('6461234567890123456').should.equal('Discover');
-  });
-  it('has a prefix of 647 and a length of 16', function(){
-      detectNetwork('6471234567890123').should.equal('Discover');
-  });
-  it('has a prefix of 647 and a length of 19', function(){
-      detectNetwork('6471234567890123456').should.equal('Discover');
-  });
-  it('has a prefix of 648 and a length of 16', function(){
-      detectNetwork('6481234567890123').should.equal('Discover');
-  });
-  it('has a prefix of 648 and a length of 19', function(){
-      detectNetwork('6481234567890123456').should.equal('Discover');
-  });
-  it('has a prefix of 649 and a length of 16', function(){
-      detectNetwork('6491234567890123').should.equal('Discover');
-  });
-  it('has a prefix of 649 and a length of 19', function(){
-      detectNetwork('6491234567890123456').should.equal('Discover');
-  });
-
-
-
-
 
 
   it('has a prefix of 6011 and a length of 16', function(){
@@ -240,7 +197,7 @@ describe('Maestro', function() {
     detectNetwork('5018123456789012').should.equal('Maestro');
   });
   it('has a prefix of 5018 and a length of 17', function(){
-    detectNetwork('5018123456780123').should.equal('Maestro');
+    detectNetwork('50181234567890123').should.equal('Maestro');
   });
   it('has a prefix of 5018 and a length of 18', function(){
     detectNetwork('501812345678901234').should.equal('Maestro');
@@ -265,7 +222,7 @@ describe('Maestro', function() {
     detectNetwork('5020123456789012').should.equal('Maestro');
   });
   it('has a prefix of 5020 and a length of 17', function(){
-    detectNetwork('5020123456780123').should.equal('Maestro');
+    detectNetwork('50201234567890123').should.equal('Maestro');
   });
   it('has a prefix of 5020 and a length of 18', function(){
     detectNetwork('502012345678901234').should.equal('Maestro');
@@ -291,7 +248,7 @@ describe('Maestro', function() {
     detectNetwork('5038123456789012').should.equal('Maestro');
   });
   it('has a prefix of 5038 and a length of 17', function(){
-    detectNetwork('5038123456780123').should.equal('Maestro');
+    detectNetwork('50381234567890123').should.equal('Maestro');
   });
   it('has a prefix of 5038 and a length of 18', function(){
     detectNetwork('503812345678901234').should.equal('Maestro');
@@ -317,7 +274,7 @@ describe('Maestro', function() {
     detectNetwork('6304123456789012').should.equal('Maestro');
   });
   it('has a prefix of 6304 and a length of 17', function(){
-    detectNetwork('6304123456780123').should.equal('Maestro');
+    detectNetwork('63041234567890123').should.equal('Maestro');
   });
   it('has a prefix of 6304 and a length of 18', function(){
     detectNetwork('630412345678901234').should.equal('Maestro');
